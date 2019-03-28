c185813f61a61f0f21a71b839d8a16437582b7fb
han xu <hanxu27@gmail.com>
2019-03-28 20:09:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,14 +8,13 @@ def speak_to_grandma(argument)
     puts "HUH?! SPEAK UP, SONNY!"  
   end
 
-u (or at least she thinks so) 
+elseif argument == argument.upcase
+  puts "NO, NOT SINCE 1938!"
+# If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
 # NO, NOT SINCE 1938!
-elseif argument == argument.upcase
-  puts "NO, NOT SINCE 1938!"
-end
-# If you shout, she can hear yo
+
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
